a1d62fa68b9fd7250019ee0ba998435b71d1cd35
inhan <korea4127@gmail.com>
2022-03-08 17:21:14 +0900

    add day components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import "./App.css";
 import { getMonth } from "./util.js";
+import CalendarHeader from "./components/CalendarHeader";
+import Sidebar from "./components/Sidebar";
+import Month from "./components/Month";
+import { useState } from "react";
+
 function App() {
-  console.table(getMonth(), "dd");
-  return <div className="App"></div>;
+  const [currentMonth, setCurrentMonth] = useState(getMonth());
+  console.table(getMonth());
+  return (
+    <>
+      <div className="h-screen flex flex-columns">
+        <CalendarHeader />
+        <div className="flex flex-1">
+          <Sidebar />
+          <Month month={currentMonth} />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Month.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Days from "./Days";
+
+function Month({ month }) {
+  return (
+    <div className="flex-1 grid grid-cols-7 grid-rows-5">
+      {month.map((row, i) => (
+        <React.Fragment key={i}>
+          {row.map((day, idx) => (
+            <Days day={day} key={idx} />
+          ))}
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
+
+export default Month;
